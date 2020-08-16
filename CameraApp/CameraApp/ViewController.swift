80f58d4f065d71783fc23d38e2f4e49f03a26cd3
daiki0730 <xcrqf313@yahoo.cp.jp>
2020-08-16 16:40:18 +0900

    start camera app

---
+++ CameraApp/CameraApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraApp
+//
+//  Created by Nishida Daiki on 2020/08/16.
+//  Copyright © 2020 Nishida Daiki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
